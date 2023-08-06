83fdd4fb6a4ba7f7dd7cb86cd1fa9c8ae480a8a2
saroj9 <saroj@distronix.in>
2023-08-06 22:11:05 +0530

    project done

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -18,7 +18,11 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
     setTimeout(() => setCopied(false), 3000);
   };
 
-  const handleProfileClick = () => {};
+  const handleProfileClick = () => {
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
 
   return (
     <div className="prompt_card">
@@ -64,6 +68,22 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
       >
         {post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
